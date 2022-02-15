0b36967e8919c44669d970c35c57e75071db3b07
Scott Gingras <greanbaby@gmail.com>
2022-02-15 16:36:55 -0700

    Form processing

--- pc_clinic_app.js
+++ pc_clinic_app.js
@@ -1,6 +1,7 @@
 /**
- * Associate Clinic, Pincher Creek, Alberta
- * pincher-creek-clinic Express 4.17.2 app
+ * pincher-creek-clinic
+ * Associate Clinic | Pincher Creek, Alberta, Canada
+ * Based on: Express 4.17.2, EJS 3.1.6
  * @name pc_clinic_app.js
  * @author Scott Gingras
  * @since 2022-Feb-13
@@ -8,48 +9,52 @@
  */
 const express = require( 'express' ),
     path = require( 'path' ),
-    bodyParser = require( 'body-parser' ),
     blogger = require( './middleware/blogger' ),
+    bodyParser = require( 'body-parser' ),
     index = require( './routes/index' );
 /**
- * PC Clinic APP
- * 1) define the port for the server and initialize express
+ * 1) PC Clinic APP
+ * ------------------------------------------------
+ * define the port for the server and initialize express
  * @member pc_clinic_app
  */
 const PORT = process.env.PORT || 3000,
     pc_clinic_app = express();
 /**
- * BLOGGER
- * 2) register the blogger middleware to blog the requests to console
+ * 2) MIDDLEWARE
+ * ------------------------------------------------
  * @implements /middleware/blogger
+ * @implements body-parser
  */
 pc_clinic_app.use( blogger() );
+pc_clinic_app.use( bodyParser.urlencoded( { extended: false, } ) );
 /**
- * VIEWS
- * 3) set views namespace to our views folder and
- * 4) instruct Express server to use EJS template engine
+ * 3) VIEWS
+ * ------------------------------------------------
+ * set views namespace to our views folder and instruct Express server to use EJS engine
  * @implements /views
  * @implements /views/EJS
  */
 pc_clinic_app.set( 'views', path.join( __dirname, 'views' ) );
 pc_clinic_app.set( 'view engine', 'ejs' );
 /**
- * STATIC /public
- * 5) register the static middleware to host the public directory
+ * 4) STATIC hosting /public
+ * ------------------------------------------------
+ * register the static middleware to host the public directory
  * @implements /express.static
  */
 pc_clinic_app.use( express.static( path.join( __dirname, 'public' ) ) );
 /**
- * / INDEX
- * 6) mount our index route
+ * 5) / INDEX ROUTE
+ * ------------------------------------------------
+ * mount our index route
  * @implements /routes/index
  */
 pc_clinic_app.use( '/', index );
 /**
- * RUN THAT
- * 7) start Express server on specified port
- * @implements PORT
+ * RUN APP
+ * ------------------------------------------------
  */
 pc_clinic_app.listen( PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log( `Server listening on port ${PORT}` );
 });
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,23 @@
  */
 const express = require('express'),
     router = express.Router();
+router.get("/:named?", (req, res) => {
+    const title = 'Express',
+        named = req.params.named;
+    res.render( 'index', {
+        title: title,
+        named: named
+    });
+});
+router.post( '/data', (req, res) => {
+    res.redirect( `/${req.body.named}` );
+});
+module.exports = router;
+/*
 router.get("/", (req, res) => {
     const title = "Clinic";
     res.render( 'index', {
         title: 'Clinic with EJS',
     });
 });
-module.exports = router;
+*/
